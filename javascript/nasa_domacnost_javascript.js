d12158dcd7d976a0f9b8662495c32d13e247f330
Tomáš Popík <74919436+tppope@users.noreply.github.com>
2020-12-18 00:40:46 +0100

    Making of title page. Not complete!

---
+++ javascript/nasa_domacnost_javascript.js
@@ -0,0 +1,17 @@
+$(window).on("load",function (){
+    document.getElementById("nebo").style.top = 0 +"%";
+    document.getElementById("grass").style.bottom = 0 +"%";
+
+    document.getElementById("middle").style.top = 19 +"%";
+    document.getElementById("middle").style.left = 40 +"%";
+
+    document.getElementById("left_up").style.top = 9.5 +"%";
+    document.getElementById("left_up").style.left = 15.7 +"%";
+
+    document.getElementById("left_down").style.bottom = 18.3 +"%";
+    document.getElementById("left_down").style.left = 0 +"%";
+
+    document.getElementById("right").style.bottom = 22 +"%";
+    document.getElementById("right").style.right = 0 +"%";
+});
+
